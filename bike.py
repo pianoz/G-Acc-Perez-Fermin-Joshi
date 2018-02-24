bec9c054c4b73f95d70e151c55d9bb4abf0c3992
pianoz <rope7883@colorado.edu>
2018-02-24 12:04:30 -0700

    Oh yeah

--- bike.py
+++ bike.py
@@ -1,28 +1,28 @@
-
 import socket
 import pygame
-import math
 
 
+# game variables
 self = pygame.init()
 rbound = 1000
 hbound = 1000
 screen = pygame.display.set_mode((rbound, hbound))
 imagemap = pygame.image.load('map.png')
-dir = 0;
 
 
+# socket variables
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+UDP_PORT = 12000
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 
 # Game code here, this currently works with keys for testing.
 
-def main(self, screen):
+def game(self, screen):
+
+    dir=0
+    # little cursor images
     imageleft = pygame.image.load('left.png')
     imageright = pygame.image.load('right.png')
     imageup = pygame.image.load('up.png')
@@ -32,38 +32,36 @@ def main(self, screen):
     imagex = 500
     imagey = 500
 
-    dval = 0;
+    image_direction_value = 0;
 
     while 1:
-
-
 
         'active keypressing'
         keys = pygame.key.get_pressed()
 
         'moving left'
         if keys[pygame.K_a] or keys[pygame.K_LEFT] or dir == 1:
-            dval = 0
+            image_direction_value = 0
             'left boundary and movement'
             if imagex > 0:
                 imagex = imagex - 3
 
         'moving right'
         if keys[pygame.K_d] or keys[pygame.K_RIGHT] or dir == 2:
-            dval = 1
+            image_direction_value = 1
             'setting boundaries and then movement'
             if imagex < rbound:
                 imagex = imagex + 3
 
         'moving up'
         if keys[pygame.K_w] or keys[pygame.K_UP] or dir == 3:
-            dval = 2
+            image_direction_value = 2
             if imagey < rbound:
                 imagey = imagey - 3
 
         'moving down'
         if keys[pygame.K_s] or keys[pygame.K_DOWN] or dir == 4:
-            dval = 3
+            image_direction_value = 3
             if imagey > 0:
                 imagey = imagey + 3
 
@@ -77,18 +75,38 @@ def main(self, screen):
 
         'player/map draw and refresh'
         screen.blit(imagemap, (0, 0))
-        if dval == 0:
+        if image_direction_value == 0:
             screen.blit(imageleft, (imagex, imagey))
-        if dval == 1:
+        if image_direction_value == 1:
             screen.blit(imageright, (imagex, imagey))
-        if dval == 2:
+        if image_direction_value == 2:
             screen.blit(imageup, (imagex, imagey))
         else:
             screen.blit(imagedown, (imagex, imagey))
         pygame.display.flip()
 
+        # listen for Wekinator
         data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-        print("received message:", data)
+        # print(data,"\n",data[20], " ",data[21])
 
-main(self,screen)
+        # DATA COMES IN FROM 20 & 21, using classifiers, you can get distinct sums which allow diferentiation
+        # ... between 1,2,3,4. If you change this to continuous, then you will have to change this.
+        tempsum = data[20] + data[21]
 
+        if tempsum == 191:
+            # result 1, classifier 1, direction left
+            dir = 1
+
+        if tempsum == 64:
+            # result 2, classifier 2 direction right
+            dir = 2
+
+        if tempsum == 128:
+            # result 3, classifier 3, direction up
+            dir = 3
+
+        if tempsum == 192:
+            # result 4, classifier 4, direction down
+            dir = 4
+
+game(self, screen)
